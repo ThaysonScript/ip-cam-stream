a8d0fe0d65f1437795c05abd1afb25367b875603
ThaysonScript <thaysonguedes01@gmail.com>
2025-08-02 17:44:41 -0300

    add - get snapshot for cam and update snapshot image test

--- core/camera.py
+++ core/camera.py
@@ -11,15 +11,16 @@ class Camera:
 
         self.camera_ip = os.getenv("CAMERA_IP")
         self.camera_user = os.getenv("CAMERA_USER")
-        self.camera_password = urllib.parse.quote(os.getenv("CAMERA_PASSWORD"), safe='')
+        self.camera_password = os.getenv("CAMERA_PASSWORD")
+        self.camera_password_formmat = urllib.parse.quote(os.getenv("CAMERA_PASSWORD"), safe='')
 
-        self.intelbras = IntelbrasAPI(f"http://{self.camera_ip}").login(
-            f"{self.camera_user}", f"{self.camera_password}"
-        )
+        self.intelbras = IntelbrasAPI(f"http://{self.camera_ip}")
+        
+        self.intelbras.login(f"{self.camera_user}", f"{self.camera_password}")
         
     
     def rtp(self):
-        rtsp_url = f"rtsp://{self.camera_user}:{self.camera_password}@{self.camera_ip}:554/cam/realmonitor?channel=1&subtype=0"
+        rtsp_url = f"rtsp://{self.camera_user}:{self.camera_password_formmat}@{self.camera_ip}:554/cam/realmonitor?channel=1&subtype=0"
         
         cap = cv2.VideoCapture(rtsp_url)
         
@@ -38,3 +39,18 @@ class Camera:
 
         cap.release()
         cv2.destroyAllWindows()
+        
+        
+    def get_snapshot(self):
+        response = self.intelbras.snapshot(channel=1, type=0)
+        
+        print(response.status_code)
+
+        if response.status_code == 200:
+            with open("img.jpeg", "wb") as bf:
+                bf.write(response.content)
+                
+                
+                
+obj = Camera()
+obj.get_snapshot()
